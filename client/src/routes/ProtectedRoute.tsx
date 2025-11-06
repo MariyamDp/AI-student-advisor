46121068b7610b342278ec1b70387e968ab62d15
Elmira Shainurova <shainurova.nursultan@gmail.com>
2025-11-06 16:10:56 +0500

    fix: remove JSX

--- client/src/routes/ProtectedRoute.tsx
+++ client/src/routes/ProtectedRoute.tsx
@@ -1,10 +1,12 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import React from 'react';
+import type { ReactNode } from 'react';
 
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { isAuthenticated } = useAuth();
   if (!isAuthenticated) return <Navigate to="/login" replace />;
-  return children as JSX.Element;
+  return children as React.ReactElement;
 };
 
 export default ProtectedRoute;
